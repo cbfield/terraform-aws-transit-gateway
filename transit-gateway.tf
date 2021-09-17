791f7ef5ffb9ae40ee3063c557634094a2f91679
Christopher Basketfield <chris.basketfield@gmail.com>
2021-09-16 17:06:22 -0800

    nametag

--- transit-gateway.tf
+++ transit-gateway.tf
@@ -1,4 +1,4 @@
-resource "aws_ec2_transit_gateway" "example" {
+resource "aws_ec2_transit_gateway" "tgw" {
   auto_accept_shared_attachments  = var.auto_accept_shared_attachments
   default_route_table_association = var.default_route_table_association
   description                     = var.description
